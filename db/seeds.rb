8d17f192d04e95dfcce15630dad83eeb30df324e
David Mandolini <damandolini@gmail.com>
2022-01-11 19:05:16 -0600

    Add seeds and generate products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+product = Product.new(:name => "TV", :price => 300, :img_url => nil, :description => "Fun box of moving pictures")
+product.save
+
+product = Product.new(:name => "PS5", :price => 400, :img_url => nil, :description => "The best console")
+product.save
+
+product = Product.new(:name => "Trash", :price => 15, :img_url => nil, :description => "Storage for your Nintendo Switch")
+product.save
