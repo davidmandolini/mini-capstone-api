70eeac5bee1585b837f48085531a3ea701b9fe36
David Mandolini <damandolini@gmail.com>
2022-01-11 19:23:46 -0600

    Add individual routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,47 @@
 class ProductsController < ApplicationController
   def all_products_method
-    
-    Product.all.each do |item|
     info = ""
-    info += "Name: #{item.name}"
-    info += "\n"
-    info += "Price: $#{item.price}"
-    info += "\n"
-    info += "Description: #{item.description}"
-    info += "\n\n\n"
+    Product.all.each do |item|
+      info += "Name: #{item.name}"
+      info += "\n"
+      info += "Price: $#{item.price}"
+      info += "\n"
+      info += "Description: #{item.description}"
+      info += "\n\n\n"
     end
-    render json: info
+    render json: info.as_json
+  end
+
+  def tv_method
+    info = ""
+    tv = Product.find_by(:name => "TV")
+    info += "Name: #{tv.name}"
+    info += "\n"
+    info += "Price: $#{tv.price}"
+    info += "\n"
+    info += "Description: #{tv.description}"
+    render json: info.as_json
+  end
+
+  def ps5_method
+    info = ""
+    ps5 = Product.find_by(:name => "PS5")
+    info += "Name: #{ps5.name}"
+    info += "\n"
+    info += "Price: $#{ps5.price}"
+    info += "\n"
+    info += "Description: #{ps5.description}"
+    render json: info.as_json
+  end
+
+  def trash_method
+    info = ""
+    trash = Product.find_by(:name => "Trash bin")
+    info += "Name: #{trash.name}"
+    info += "\n"
+    info += "Price: $#{trash.price}"
+    info += "\n"
+    info += "Description: #{trash.description}"
+    render json: info.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/all_products", controller: "products", action: "all_products_method"
+  get "/tv", controller: "products", action: "tv_method"
+  get "/ps5", controller: "products", action: "ps5_method"
+  get "/trash", controller: "products", action: "trash_method"
   # Defines the root path route ("/")
   # root "articles#index"
 end
