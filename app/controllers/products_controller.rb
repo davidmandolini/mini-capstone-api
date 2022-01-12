9b8b8dc7c4c49a789f17019f7ea6820d60fe61ef
David Mandolini <damandolini@gmail.com>
2022-01-11 19:13:40 -0600

    Add all_products route

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,14 @@
 class ProductsController < ApplicationController
+  def all_products_method
+    
+    Product.all.each do |item|
+    info = ""
+    info += "Name: #{item.name}"
+    info += "\n"
+    info += "Price: $#{item.price}"
+    info += "\n"
+    info += "Description: #{item.description}"
+    info += "\n\n\n"
+    end
+    render json: info
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get "/all_products", controller: "products", action: "all_products_method"
   # Defines the root path route ("/")
   # root "articles#index"
 end
