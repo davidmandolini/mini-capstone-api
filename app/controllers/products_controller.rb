2a5af507ac0756a3d7a95989999b694a9bf28cad
David Mandolini <damandolini@gmail.com>
2022-01-12 20:21:24 -0600

    Add one_product route

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,36 +12,14 @@ class ProductsController < ApplicationController
     render json: info.as_json
   end
 
-  def tv_method
+  def product_method
     info = ""
-    tv = Product.find_by(:name => "TV")
-    info += "Name: #{tv.name}"
+    product = Product.find_by(:id => "#{params["id"]}")
+    info += "Name: #{product.name}"
     info += "\n"
-    info += "Price: $#{tv.price}"
+    info += "Price: $#{product.price}"
     info += "\n"
-    info += "Description: #{tv.description}"
-    render json: info.as_json
-  end
-
-  def ps5_method
-    info = ""
-    ps5 = Product.find_by(:name => "PS5")
-    info += "Name: #{ps5.name}"
-    info += "\n"
-    info += "Price: $#{ps5.price}"
-    info += "\n"
-    info += "Description: #{ps5.description}"
-    render json: info.as_json
-  end
-
-  def trash_method
-    info = ""
-    trash = Product.find_by(:name => "Trash bin")
-    info += "Name: #{trash.name}"
-    info += "\n"
-    info += "Price: $#{trash.price}"
-    info += "\n"
-    info += "Description: #{trash.description}"
+    info += "Description: #{product.description}"
     render json: info.as_json
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "/all_products", controller: "products", action: "all_products_method"
-  get "/tv", controller: "products", action: "tv_method"
-  get "/ps5", controller: "products", action: "ps5_method"
-  get "/trash", controller: "products", action: "trash_method"
+  get "/all_products" => "products#all_products_method"
+  get "/one_product" => "products#product_method"
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
