c6fd9b09e075fc3dffd287257e0e21ae74134a70
David Mandolini <damandolini@gmail.com>
2022-01-13 19:18:22 -0600

    gemfile update

--- testing_script.rb
+++ testing_script.rb
@@ -1,7 +1,8 @@
 require "http"
 
-response = HTTP.post("http://localhost:3000/products", :form => { :name => "Sega Saturn", :price => 150, :description => "A classic" })
+response = HTTP.get("http://localhost:3000/products/1")
+message = JSON.parse(response.body)
+p message["name"]
+#response = HTTP.patch("http://localhost:3000/products/2", :form => { :name => "PS5", :price => 300, :description => "The best console" })
 
-response = HTTP.patch("http://localhost:3000/products/2", :form => { :name => "PS5", :price => 300, :description => "The best console" })
-
-response = HTTP.delete("http://localhost:3000/products/5")
+#response = HTTP.delete("http://localhost:3000/products/5")
