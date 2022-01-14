5b12c4267f0b4e7ff991e5be3ee339f16cd79b04
David Mandolini <damandolini@gmail.com>
2022-01-13 18:44:43 -0600

    Add delete action to routes, controller, requests, and testing script

--- testing_script.rb
+++ testing_script.rb
@@ -2,4 +2,6 @@ require "http"
 
 response = HTTP.post("http://localhost:3000/products", :form => { :name => "Sega Saturn", :price => 150, :description => "A classic" })
 
-response = HTTP.patch("http://localhost:3000/products/1", :form => { :name => "Sega Genesis", :price => 150, :description => "A classic" })
+response = HTTP.patch("http://localhost:3000/products/1", :form => { :name => "TV", :price => 150, :description => "A fun box of moving pictures." })
+
+response = HTTP.delete("http://localhost:3000/products/6")
