a8525a64f83e86861d6b2db4a201631db8fbcf1d
David Mandolini <damandolini@gmail.com>
2022-01-17 20:48:03 -0600

    Add validation to supplier model

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,2 +1,5 @@
 class Supplier < ApplicationRecord
+  validates :name, :presence => true, :uniqueness => true
+  validates :email, :presence => true
+  validates :phone_number, :length => { :minimum => 10, :maximum => 20}
 end
