6681e563a26687805305dc3eb7565ada15943f51
David Mandolini <damandolini@gmail.com>
2022-01-18 19:11:17 -0600

    modify product update action to accept supplier_id

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -38,6 +38,7 @@ class ProductsController < ApplicationController
     product.name = params["name"] || product.name
     product.price = params["price"] || product.price
     product.description = params["description"] || product.description
+    product.supplier_id = params["supplier_id"] || product.supplier_id
     if product.save
       render json: product
     else
