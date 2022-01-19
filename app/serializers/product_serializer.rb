83964db3c3523a0413d087101545afb38de4cbf6
David Mandolini <damandolini@gmail.com>
2022-01-18 20:00:50 -0600

    update product serializer with images

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,3 +1,3 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :name, :price, :tax, :total, :is_discounted?, :description, :images, :supplier_id, :supplier
+  attributes :id, :name, :price, :tax, :total, :is_discounted?, :description, :images, :supplier_id, :supplier, :images
 end
