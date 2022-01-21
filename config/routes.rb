79904c3596d55c89ab445e6bcb11a7125f5801ce
David Mandolini <damandolini@gmail.com>
2022-01-20 19:06:57 -0600

    Add orders create action

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   post "/sessions" => "sessions#create"
 
+  post "/orders" => "orders#create"
+  get "/orders" => "orders#show"
+
   get "/products" => "products#index"
   post "/products" => "products#create"
   get "/products/:id" => "products#show"
