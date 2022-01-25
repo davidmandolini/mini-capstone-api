c89a01118df5c0805bcd6de05f5e2bb6ccff97b7
David Mandolini <damandolini@gmail.com>
2022-01-24 19:38:14 -0600

    Add has_many has_many associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,11 +1,4 @@
 class Category < ApplicationRecord
   has_many :category_products
-
-  def products
-    result = []
-    category_products.each do |category_product|
-      result << category_product.product
-    end
-    return result
-  end
+  has_many :products, through: :category_products
 end
--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,3 +1,5 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :name, :price, :tax, :total, :is_discounted?, :description, :images, :supplier_id, :supplier, :images
+  attributes :id, :name, :price, :tax, :total, :is_discounted?, :description, :supplier_id
+  belongs_to :supplier, :images
+  has_many :categories
 end
