a3d2cb1525f7055a733a033e82512a21e84bd138
David Mandolini <damandolini@gmail.com>
2022-01-24 19:18:05 -0600

    Add products method to Category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,12 @@ class Category < ApplicationRecord
   has_many :category_products
 
   def products
-    CategoryProduct.where(:category_id => id)
+    category_products = CategoryProduct.where(:category_id => id)
+    products = []
+    category_products.each do |item|
+      product = Product.find_by(:id => item.product_id)
+      products << product
+    end
+    return products
   end
 end
