b64ca3aedb3c6e8985c23f5b0e08d73bf8ffc2ac
David Mandolini <damandolini@gmail.com>
2022-02-22 11:38:23 -0600

    authenticate user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,7 @@ class ApplicationController < ActionController::API
 
   def authenticate_admin
     unless (current_user && current_user.admin)
-      render json: {}, status: :unauthorized
+      render json: { error: "Not authorized" }, status: :unauthorized
     end
   end
 end
